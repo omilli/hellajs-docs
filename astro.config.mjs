5d5174814bb7c050e3cdec24e862bb8776d41d6f
omilli <oli.millington@gmail.com>
2025-05-26 09:37:26 +0700

    Refactor and enhance documentation for various components and APIs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,10 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'HellaJS',
+			logo: {
+				src: '/public/logo.svg',
+				alt: 'HellaJS Logo',
+			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/omilli/hellajs' }],
 			customCss: ['./src/styles/global.css'],
 			sidebar: [
